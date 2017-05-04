d950370b3726c335c01e63d95e7b745998350291
Rafael Brito Campos <campos.rb@hotmail.com>
2017-05-04 21:00:41 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,15 +2,15 @@ var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+    it('returns "Michal Jackson"', () => {
+      expect(index.name).toEqual('Michael Jackson')
     })
   })
 
 
   describe('Height', () => {
     it('is greater than 40', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(index.height).toBeGreaterThan(40)
     })
   })
 
@@ -22,4 +22,3 @@ describe('what-is-a-test', () => {
   })
 
 })
-
