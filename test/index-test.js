d8934cf217856bb44a09bd0d6a2709b8afaed5a0
Scott Connelly <sconn0088@gmail.com>
2017-07-13 13:28:25 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 var index = require("../index.js")
 
-/*
+/* hi
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -23,4 +23,3 @@ describe('what-is-a-test', () => {
   })
 })
 */
-
