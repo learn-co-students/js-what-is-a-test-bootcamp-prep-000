c5c15f1189f75e013ad0808e602b5bd65418b049
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-07-21 02:29:38 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,5 @@
 var index = require("../index.js")
 
-/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -10,7 +9,7 @@ describe('what-is-a-test', () => {
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
+    it('is less than 80', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
@@ -22,5 +21,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
-
