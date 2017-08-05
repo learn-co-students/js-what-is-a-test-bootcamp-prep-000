c2315aad79b34882725fe415f3a3f9fb58e43283
Vanessa George <vanessa.s.george@gmail.com>
2017-08-05 19:30:40 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+
+if (name === "Susan") {console.log("This is correct")} else {console.log("Expected name to be Susan")}
+
+if(height < 40) {console.log("This is correct")} else {console.log("Expected height to be less than 40")}
+
+console.log(message)
