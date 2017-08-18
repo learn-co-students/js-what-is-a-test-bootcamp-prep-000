b4ff99e2580ea788430de3349736fb9651eda835
EPIC448 <samrey2018@gmail.com>
2017-08-18 12:10:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
+/* USING VARABLES TO CREATE SENTANCES */
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-
+/* Everytime you do anything, you must save your work */
 module.exports = {name, height, message}
+ console.log (message)
