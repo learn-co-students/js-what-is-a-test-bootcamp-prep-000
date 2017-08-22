8ed3d2d21e301b0ca1022816ad00fa1590cf8813
Brandon Brasson <rocksong622@gmail.com>
2017-08-22 16:35:31 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,3 +4,24 @@ var name ="Brandon"
 var height = "71"
 
 var message = `${name}Brandon ${height} 71 inches tall`
+
+module.exports = {name, height, message}
+
+escribe('Brandon', () => {
+    it('returns "Brandon"', () => {
+      expect(index.name).toEqual('Brandon')
+    })
+  })
+
+  cribe("Height", ("71") => {
+      it("is less than 71", ("71") => {
+        expect(index.height).toBeLessThan("71")
+      })
+    })
+
+    describe("Message", () => {
+    it("Brandon", ("Brandon") => {
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
+    })
+  })
