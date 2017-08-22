2db7e44872a7fad7d49fae35a1800268c8ab84f9
Brandon Brasson <rocksong622@gmail.com>
2017-08-22 16:15:09 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,26 +1,6 @@
 var index = require("../index.js")
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
+var name ="Brandon"
+var height = "71"
 
-
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
-    })
-  })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
-})
-*/
-
+var message = `${name}Brandon ${height} 71 inches tall`
