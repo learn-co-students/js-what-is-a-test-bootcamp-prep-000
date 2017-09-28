549ab4a3edd3d5ace38afca8054a96ad7038f500
Derya Akbaba <deryakbaba22@gmail.com>
2017-09-28 17:34:08 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
-//module.exports = {name, height, message}
+module.exports = {name, height, message}
 
 console.log(message)
