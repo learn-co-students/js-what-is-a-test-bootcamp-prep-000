75171b9de610bae97d36893fe9cec01bf9fbcecf
Kyle Matthew Javier Cortez <kyle4346@gmail.com>
2017-09-29 23:21:19 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,26 +1,24 @@
 var index = require("../index.js")
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+      expect(index.name).toEqual('Joe')
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(75)
     })
   })
 
-  describe('Message', () => {
+  describe("Message", () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
   })
 })
-*/
-
