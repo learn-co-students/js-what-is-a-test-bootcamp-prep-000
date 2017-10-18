3e6dd776c9f95f7dc0cd5c7bfca0a966d76b0b4f
Andrew K <ajkloecker@outlook.com>
2017-10-18 00:03:08 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,6 @@
 var index = require("../index.js")
-
-/*
-describe('what-is-a-test', () => {
+var help = "none"
+/*describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
@@ -21,6 +20,4 @@ describe('what-is-a-test', () => {
       expect(index.message).toInclude(index.height)
     })
   })
-})
-*/
-
+})*/
