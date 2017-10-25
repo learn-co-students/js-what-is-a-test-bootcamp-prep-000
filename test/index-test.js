8a92351d610ed93e3d873625481dec2108bf6b8f
Rhonda Hernandez <rhondahz70@gmail.com>
2017-10-25 17:33:40 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,9 @@
 var index = require("../index.js")
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
+    it('returns "susan"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
@@ -22,5 +22,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
-
