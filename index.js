8d65feeb3a3648c056bbbb9684c7eca2f2992320
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 04:45:52 +0000

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
+function Name(name){
+if (name === 'Susan'){
+    return 'Susan'
+}else{
+  return 'We expect the name to equal Susan.'
+}}
+
+
+function Height(height){
+  if( height < 40){
+    return `Height is  ${height}`
+  }else {
+    return 'We expect the height to be less than 40.'
+  }
+}
+
+function Message(message){
+return message
+}
+
+
+
 module.exports = {name, height, message}
