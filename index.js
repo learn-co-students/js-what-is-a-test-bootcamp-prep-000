4ab2959953e0fd50a3ac2cd34c482e7e892a1a7c
Collin Semien <silverninetwofive@gmail.com>
2017-12-23 20:02:04 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+ describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(index.name).toEqual('Susan')
+    })
+  })
+    describe("Height", () => {
+    it("is less than 40", () => {
+      expect(index.height).toBeLessThan(40)
+    })
+  })
+    describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
+    })
+  })
