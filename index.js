9c3c35dc3342124eaaa3ad187bc86e91d3dd5cad
William Robertson <william02robertson@gmail.com>
2018-01-01 10:34:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// write your code below!
-var name = "Joe"
-var height = 74
-var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
+// Define Variables
+var name = "Joe";
+var height = 74;
+var message = `${name} is ${height} inches tall`;
+// Call Variables
+console.log(message);
+// Export Information
+module.exports = {name, height, message};
