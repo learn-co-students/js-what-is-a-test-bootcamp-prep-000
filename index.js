adc65f9e9eb1b57b65067e8bd04f42c95b4abb1e
Gary Morford <morfordg@mst.edu>
2018-01-10 02:56:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+clear
 // write your code below!
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+console.log(message)
