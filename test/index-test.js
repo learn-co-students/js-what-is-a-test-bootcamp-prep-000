3fdaf545e638ae2f35bd816b25ba6b1685da636b
leensan chong <linsan2304@gmail.com>
2018-01-12 23:58:03 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,17 @@
 var index = require("../index.js")
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+      expect(index.name).toEqual('Joe')
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(74)
     })
   })
 
@@ -22,4 +22,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
