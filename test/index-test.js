6fc9b15a24733a84cc32aef60a40526151896bb5
Jeremy Krumsick <jmkrumsick@gmail.com>
2018-02-08 19:40:15 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,14 +1,13 @@
 var index = require("../index.js")
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
-
-
+  
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
@@ -22,5 +21,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
-
