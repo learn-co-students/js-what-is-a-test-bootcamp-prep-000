f12f9f2596177fb8f1e3c762d4ba89c500971229
Salehe Kungule <s.kungule@irdp.ac.tz>
2018-02-18 12:09:10 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
 
 console.log(message);
+<<<<<<< HEAD
+=======
+
+>>>>>>> c7f6d1b1fe70115e23b6b0999bf059b7e5daa1b0
