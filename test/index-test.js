ff7aaead3bf4832568553e119ee3c074835d26f2
Leon K <leonknight@outlook.com>
2018-02-19 13:47:20 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,13 @@
 var index = require("../index.js")
 
-/*
+/* 
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
-
+change
 
   describe('Height', () => {
     it('is less than 40', () => {
