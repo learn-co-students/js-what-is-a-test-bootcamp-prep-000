c77d6a2aa250adedda9113c46dcea2098331e0a7
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-01 23:12:20 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -9,15 +9,16 @@ expect (index.name).toEqual('Susan')
       })
   })
 
-  describe("Height", () => {
-    it("is less than 40", () => {
-      expect(index.height).toBeLessThan(40)
+describe("Height", () => {
+    it("is toBeLessThan 40, 
+    () => {
+expect(index.height).toBeLessThan(40)
     })
   })
 
-  describe("message, () => {
-    it("gives the name and height", () => {
-    expect(index.message).toInclude(index.height)
+describe("message, () => {
+  it("gives the name and height", () => {
+  expect(index.message).toInclude(index.height)
     })
   })
 */
