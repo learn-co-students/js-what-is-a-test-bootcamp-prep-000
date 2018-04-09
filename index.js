25cdc4eb7fc81e01ccaf3f6936ef07f33edcd7d7
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-09 21:08:40 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
+/*
 module.exports = {name, height, message}
+<<<<<<< HEAD
 console.log(message)
+=======
+console.log(message)
+>>>>>>> fea9b7711c4ae04fb53b1ef8fc9f4b6eba0a6668
+
+console.log(message);
+*/
+
+console.log(message);
