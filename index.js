ff7fa1fbc1300d070ad2430a7fe339d89c5071b6
Retno Suci Wulandari <retnosw389@gmail.com>
2018-05-12 16:10:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
+var nickName = "Joe"
 var height = 74
-var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
+var message = `${nickName} is ${height} inches tall`
+console.log(message)
+module.exports = {nickName, height, message}
