f07a4cea7093e5d74552fe8de8111b4a1eaee4f4
Nathan Hill <nateohill@gmail.com>
2018-05-18 17:45:37 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
+var error = "The learn submit command requires me to have made a change so here you go :)"
 
 module.exports = {name, height, message}
