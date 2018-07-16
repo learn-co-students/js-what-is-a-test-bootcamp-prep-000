1bc7e724802897b315fa4db0e32d614f53ef9607
elaitman1 <elaitman1@gmail.com>
2018-07-16 00:47:43 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -7,8 +7,14 @@ describe('what-is-a-test', () => {
       expect(index.name).toEqual('Susan')
     })
   })
+// write your code below! //////////////
+var name = "Joe"
+var height = 74
+var message = `${name} is ${height} inches tall`
 
-
+module.exports = {name, height, message}
+console.log(message)
+//////////////////////////
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
