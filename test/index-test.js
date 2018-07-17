c10c8703a110d25753b7a8d745d8d73068301003
Travis Avey <avey.travis@gmail.com>
2018-07-17 17:49:47 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,5 @@
 var index = require("../index.js")
 
-/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -22,5 +21,5 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
 
