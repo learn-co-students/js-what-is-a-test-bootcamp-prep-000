670dd9a656ccb3033ccafbce40cbd87d9edb3ac1
Cara Dennis <ephemere@protonmail.com>
2018-07-31 19:32:22 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,6 +2,7 @@ var index = require("../index.js")
 
 /*
 describe('what-is-a-test', () => {
+
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
@@ -21,6 +22,7 @@ describe('what-is-a-test', () => {
       expect(index.message).toInclude(index.height)
     })
   })
+
 })
 */
 
