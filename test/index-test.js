a2d483083ee9a27efa77f39dbea949f27c8cb47f
Roundabout Rider <ntm.email@icloud.com>
2018-08-04 11:31:03 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,12 +8,12 @@ describe('what-is-a-test', () => {
     })
   })
 
-
+ 
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
-  })
+  }) 
 
   describe('Message', () => {
     it('gives the name and height', () => {
