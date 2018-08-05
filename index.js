4b96e15f6218a7e343e68aafe769fac877a09105
Breaun <pynkylynn@gmail.com>
2018-08-05 18:21:38 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,26 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-
+console.log(message)
 module.exports = {name, height, message}
-describe (‘name‘,()=> {it (‘returns “”,()))
+var index = require(“.../index.js”)
+
+/* 
+describe(“what-is-a-test”,() => {
+  it(kreturnd “susan”’,()=> {
+    expect(index.name).toEqual(”susan”)
+    
+  })
+})
+
+describe(” height”) => { it (”is less than 40” , () => { 
+expect(index.height).toBeLessThan(40)
+})
+})
+
+describe(”message” , () => {
+  it(“gives the name and height”, () => { expect(index.message).toinclude(index.name)
+  expect(index.message).toInclude(index.height)
+})
+
+})
