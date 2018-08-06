71ebc39e6a3a7c786351f122682157b0e5e0deec
kevin.d.tyo@gmail.com <kevin.d.tyo@gmail.com>
2018-08-06 22:43:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 var index = require("../index.js")
 
 /*
-describe('what-is-a-test', () => {
+ksd describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
