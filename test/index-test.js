490531401c06bec5c335be083da864d06cb1a78a
vincent ndisi <vndis1298@yahoo.com>
2018-08-18 00:26:42 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,10 @@
 var index = require("../index.js")
 
-/*
+
 describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+  describe('name', () => {
+    it('returns "susan"', () => {
+      expect(index.name).toEqual('susan')
     })
   })
 
@@ -22,5 +22,5 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
 
