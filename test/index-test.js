0fac5d76ad369de13973a19ebd39bb58c46b4f0c
Meet Shah <meetdshah007@gmail.com>
2019-02-05 13:05:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -14,13 +14,6 @@ describe('what-is-a-test', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
 })
 */
 
