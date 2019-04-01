5107aa4b41243bb625c8dfb3ef5a6df858d82470
RAJA SOMALINGA K <rajakesava46@gmail.com>
2019-04-01 15:13:22 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ var message = `${name} is ${height} inches tall `;
  console.log (message);
 
 module.exports = {name, height, message};
+
+
+/*  
+describe ('Name'),() => { ;
+ it ('returns "Susan"'), () => { ;
+  expect (index.name). to Equal (Susan);
+})
+})
+*/
+console.log(message);
+/*
+describe("Message", () => {  ;
+it ("gives the name and height", () +> { ;
+
+expecct (index.message). to Include (index.name)
+
+expect (Index.message).toInclude (index.height)
+})
+})
+*/
