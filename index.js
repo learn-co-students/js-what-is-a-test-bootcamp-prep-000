aa17c9b85eb0d0fa216c3e720e23ee9c5c8b06b7
Irina Bright <missirinabright@gmail.com>
2019-04-11 22:01:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // write your code below!
-var index = require("../index.js");
-describe("Message", () => {
-    it("gives the name and height", () => {
-      expect(index.message).toInclude(index.name);
-      expect(index.message).toInclude(index.height);
-    });
-  });
-  
-  console.log(message);
+var name = "Joe"
+var height = "74"
+var message = `${name} is ${height} inches tall`
+ 
+module.exports = {name, height, message}
+console.log(message)
