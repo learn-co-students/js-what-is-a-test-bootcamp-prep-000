85f284085c252b37eb854b23c655a8dd5d257231
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-04-25 00:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
-var name = "Joe"
-var height = 74
+var name = "name"
+var height = "height"
 var message = `${name} is ${height} inches tall`
+
+console.log(message)
 
 module.exports = {name, height, message}
