130c47e3e1a74aa351c84e021b0b39158eadca0d
Amy Franz <amy.franz@gmail.com>
2019-11-28 11:59:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = `${name} is ${height} inches tall`
+var name = "Joe";
+var height = 74;
+var message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message}
+
+
+function happyHolidays () {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (name) {
+  return "Happy holidays, ${name}!";
+}
+
+function happyHolidayTo (name, holiday) {
+  return "Happy ${holiday}, ${name}!";
+}
+
+function holidayCountdown (holiday, days) {
+  return "It's ${days} days until ${holiday}!";
+}
