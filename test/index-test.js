ddbffb7d136a007eb234b41aaef60156d16ce83a
zaid85j <zaid85j@gmail.com>
2020-03-23 17:40:25 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-var index = require("../index.js")
+var index = require('../index.js')
 
 
 describe('what-is-a-test', () => {
@@ -9,14 +9,14 @@ describe('what-is-a-test', () => {
   })
 
 
-  describe('Height', () => {
-    it('is less than 40', () => {
+   describe("Height", () => {
+    it("is less than 39", () => {
       expect(index.height).toBeLessThan(40)
     })
   })
 
-  describe('Message', () => {
-    it('gives the name and height', () => {
+  describe("Message", () => {
+    it("gives the name and height", () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
