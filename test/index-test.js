f8b4f8bee4a5f642fd8fa9f8589579a84df30f6b
Paul Hansen <paulrhansen@gmail.com>
2020-03-27 03:36:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,10 +2,10 @@ var index = require("../index.js")
 
 /*
 describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
+ 
       expect(index.name).toEqual('Susan')
-    })
+    } describe('Name', () => {
+    it('returns "Susan"', () => {)
   })
 
 
